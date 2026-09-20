03095f5c4865f12e311a0ca643ab8b6bd596a6de
Rishika Bansal <147906803+rishikaabansal@users.noreply.github.com>
2026-09-20 18:01:00 +0530

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.cs
@@ -0,0 +1,20 @@
+public class Solution
+{
+    public int[] ProductExceptSelf(int[] nums)
+    {
+        int[] result = new int[nums.Length];
+        int prefix = 1;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            result[i] = prefix;
+            prefix *= nums[i];
+        }
+        int postfix = 1;
+        for (int i = nums.Length - 1; i >= 0; i--)
+        {
+            result[i] *= postfix;
+            postfix *= nums[i];
+        }
+        return result;
+    }
+}
